9d065ad9cd38206a7059be84240cc94362a8caeb
Beaussier Arnaud <arnaud.beaussier@synograph.com>
2019-09-25 22:53:44 +0800

    add gcp-py-functions

---
+++ monolith-serverless-bank-example/gcp-py-functions/functions/main.py
@@ -0,0 +1,10 @@
+import os
+
+def creerUnCompte(nomDuCompte):
+  return "Compte %s crée." % (nomDuCompte)
+
+def supprimerUnCompte(nomDuCompte):
+  return "Compte %s supprimé." % (nomDuCompte)
+
+def consulterUnCompte(nomDuCompte):
+  return "Solde du compte: %s Euros" % ()
