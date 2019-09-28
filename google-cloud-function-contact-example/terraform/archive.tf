ad421a881a0020850c460f44c902b45dd8cb3dcc
Beaussier Arnaud <arnaud.beaussier@synograph.com>
2019-09-28 18:07:17 +0800

    update archive_fiel

--- google-cloud-function-contact-example/terraform/archive.tf
+++ google-cloud-function-contact-example/terraform/archive.tf
@@ -1,14 +1,6 @@
 data "archive_file" "contactFormSource" {
   type        = "zip"
-  output_path = "${path.module}/contactForm/contactFormSource.zip"
+  output_path = "${path.module}/contactFormSource.zip"
 
-  source {
-    content  = "${path.module}/contactForm/main.py"
-    filename = "main.py"
-  }
-
-  source {
-    content  = "${path.module}/contactForm/requirements.py"
-    filename = "requirements.txt"
-  }
+  source_dir = "${path.module}/contactForm"
 }
