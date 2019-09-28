74f2f180a7ce8c4a17034d1fb428b4b085d5c425
Beaussier Arnaud <arnaud.beaussier@synograph.com>
2019-09-28 17:51:00 +0800

    update function, fix runtime typo

--- google-cloud-function-contact-example/terraform/main.tf
+++ google-cloud-function-contact-example/terraform/main.tf
@@ -1,19 +1,20 @@
 resource "google_storage_bucket" "contactFormSource" {
-  name = "contact-form-source-bucket"
+  name = "contact-form-source"
 }
 
 resource "google_storage_bucket_object" "contactFormSource" {
   name   = "contactFormSource.zip"
   bucket = "${google_storage_bucket.contactFormSource.name}"
-  source = data.archive.contactFormSource.name
+  source = data.archive_file.contactFormSource.output_path
 }
 
 resource "google_cloudfunctions_function" "contactForm" {
   name                  = "contactForm"
   description           = "Handle the Contact Form of a website"
-  runtime               = "python3.7"
+  runtime               = "python37"
   max_instances         = 5
   available_memory_mb   = 128
+  timeout               = 10
   source_archive_bucket = "${google_storage_bucket.contactFormSource.name}"
   source_archive_object = "${google_storage_bucket_object.contactFormSource.name}"
   trigger_http          = true
